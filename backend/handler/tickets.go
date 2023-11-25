3a07c72c19a9c6efe6d66163aca65ba91cc36bf8
Arelthia Phillips <1953051+arelthia@users.noreply.github.com>
2023-11-25 00:20:05 -0600

    add cors support

--- backend/handler/tickets.go
+++ backend/handler/tickets.go
@@ -11,6 +11,19 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func GetTickets(w http.ResponseWriter, r *http.Request) {
 	dbRunner := data.GetDb()
 	tickets, err := dbRunner.GetTickets()
